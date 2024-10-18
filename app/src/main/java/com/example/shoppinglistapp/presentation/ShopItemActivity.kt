0168942105f050bea2d0632df3fb3cf5fa6624e7
Pave1iOS <PGribachev94@yandex.ru>
2024-10-18 10:51:15 +0300

    create ListAdapter

---
+++ app/src/main/java/com/example/shoppinglistapp/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppinglistapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShopItemActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
